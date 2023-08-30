108369ecfb26d767b40785d1d8794500d0e97515
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-31 01:41:44 +0530

    Correct iteration for a simpler concise logic, updated some class names, Coded display updation for number buttons of calculator

---
+++ main.js
@@ -0,0 +1,68 @@
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b){
+    if(b===0){
+        return alert(`You can't divide by 0`)
+    }else{
+        return a/b;
+    }
+}
+
+function operate(operator,a,b){
+    switch (operator) {
+        case "+":
+          return add(a, b);
+        case "-":
+          return subtract(a, b);
+        case "*":
+          return multiply(a, b);
+        case "÷":
+          return divide(a, b);
+        default:
+          throw new Error("Invalid operator");
+      }
+ }
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    let buttons = document.querySelectorAll('button')
+    let display = document.querySelector('.display')
+    let operators = document.querySelector('.operator')
+    let currentNumber = ""
+    let firstNumber = null
+    let currentOperator =null
+
+    buttons.forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const buttonValue = button.textContent;
+
+            if(!isNaN(buttonValue)||buttonValue==='.'){
+                currentNumber += buttonValue;
+                updateDisplay();
+            }
+
+
+
+
+
+
+
+
+
+        })
+    })
+    function updateDisplay() {
+        display.textContent = currentNumber || '0';
+    }
+
+})
